7c3f419ef66a895c3e3191362889025ef473ab4d
myin <minyin142p@gmail.com>
2024-10-07 16:01:47 +0200

    \                     /

--- godot-variables.sh
+++ godot-variables.sh
@@ -2,11 +2,15 @@
 
 GAME_NAME=$1
 GODOT_VERSION=$2
+STEAM_APP=$3
+GAME_CODE=$4
 
 gh variable set GAME_NAME -b $GAME_NAME
 gh variable set GODOT_VERSION -b $GODOT_VERSION
+gh variable set STEAM_APP -b $STEAM_APP
 
 CODE=$(openssl rand -hex 32)
 gh secret set ENCRYPTION_KEY -b $CODE
+gh secret set GAME_CODE -b $GAME_CODE
 
 echo "Set variables for game"
