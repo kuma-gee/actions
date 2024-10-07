05cdddbff52fe1f2564a1587a21228c423452693
myin <minyin142p@gmail.com>
2024-10-07 13:01:01 +0200

    /

--- godot-variables.sh
+++ godot-variables.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 GAME_NAME=$1
+GODOT_VERSION=$2
 
 gh variable set GAME_NAME -b $GAME_NAME
+gh variable set GODOT_VERSION -b $GODOT_VERSION
 
 CODE=$(openssl rand -hex 32)
 gh secret set ENCRYPTION_KEY -b $CODE
