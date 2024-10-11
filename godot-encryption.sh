e92532f8931dfedc2caf82b14b250aafc3cb6b5e
myin <minyin142p@gmail.com>
2024-10-11 15:27:36 +0200

    update to use inputs

--- godot-encryption.sh
+++ godot-encryption.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
-GODOT_VERSION=$1
-
-if [ ! -z "$GODOT_VERSION" ]; then
-    gh variable set GODOT_VERSION -b $GODOT_VERSION
-fi
-
 CODE=$(openssl rand -hex 32)
 gh secret set ENCRYPTION_KEY -b $CODE
